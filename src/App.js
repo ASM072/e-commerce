fe32f0d6148cfc83ab69ab19d24ffdac0d16c59b
ASM072 <ahmedsaadmalik8@gmail.com>
2023-07-31 20:30:10 +0500

    created div for containers, css of categoriesdiv and images, defined categories and styling of category containers

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
-// eslint-disable-nextline
+import './categories.style.scss';
 
-import './App.css';
-import { Component } from 'react';
-
-class App extends Component {
-  
-  constructor ()
-  {
-    super();
-    this.state = {
-     
-    };
-  }
-
-  componentDidMount ()
-  {
-    
-  }
-
-  render ()
-  {
-    return (
-      <div className="App">
-        <input className='search-box' type='search' placeholder='Search Watches' onChange=
-          {
-          ( event ) =>
-          {
-          console.log( event );
-          }
-          }>
-          
-        </input>
-      </div>
-    );
-  }
-}
+const App = () =>
+{
+  const categories = [
+    {
+      id: 1,
+      title: 'Brands',
+      imageUrl: 'https://i.ibb.co/0ZWXmr7/Brands.png'
+    },
+    {
+      id: 2,
+      title: 'Accessories',
+      imageUrl: 'https://i.ibb.co/w4jcdyj/Accessories.png'
+    },
+    {
+      id: 3,
+      title: 'Men',
+      imageUrl: 'https://i.ibb.co/k9krbS8/Men.png'
+    },
+    {
+      id: 4,
+      title: 'Women',
+      imageUrl: 'https://i.ibb.co/FDvdCfQ/Women.png'
+    },
+    {
+      id: 5,
+      title: 'Kids',
+      imageUrl: 'https://i.ibb.co/TRC6XBQ/Kids.png'
+    }
+  ]
+  return (
+    <div className="categoriesContainer">
+      { categories.map( ( { title, id, imageUrl } ) => (
+        <div key={ id } className="categoryContainer">
+          <div className="backgroundImage" style={ {
+            backgroundImage: `url(${ imageUrl })`
+          } }/>
+          <div className="categoryBodyContainer">
+            <h2>{ title }</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
