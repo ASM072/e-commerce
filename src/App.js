66b94625f475cae0f38a7fdcbdbce2c827dcb2a1
ASM072 <ahmedsaadmalik8@gmail.com>
2023-07-31 22:39:23 +0500

    added react router and created separate component for home page

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
-
-import Directory from './Components/directory/directory.component';
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/routes/home/home.component";
 
 const App = () =>
 {
-  const categories = [
-    {
-      id: 1,
-      title: 'Brands',
-      imageUrl: 'https://i.ibb.co/0ZWXmr7/Brands.png'
-    },
-    {
-      id: 2,
-      title: 'Accessories',
-      imageUrl: 'https://i.ibb.co/w4jcdyj/Accessories.png'
-    },
-    {
-      id: 3,
-      title: 'Men',
-      imageUrl: 'https://i.ibb.co/k9krbS8/Men.png'
-    },
-    {
-      id: 4,
-      title: 'Women',
-      imageUrl: 'https://i.ibb.co/FDvdCfQ/Women.png'
-    },
-    {
-      id: 5,
-      title: 'Kids',
-      imageUrl: 'https://i.ibb.co/TRC6XBQ/Kids.png'
-    }
-  ]
   return (
-   <Directory categories={categories}/>
-  );
+    <Routes>
+      <Home />
+    </Routes>
+    
+)
 };
-
 export default App;
