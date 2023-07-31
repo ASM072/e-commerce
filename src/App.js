7543c462b1f4d699ced8d26988170bf37cc87d8a
ASM072 <ahmedsaadmalik8@gmail.com>
2023-08-01 00:06:06 +0500

    naivgation function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./Components/routes/home/home.component";
+
+const Navigation = () =>
+{
+  return (
+    <div>
+      <Outlet/>
+    </div>
+  )
+}
 
 const App = () =>
 {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}> </Route>
+      <Route path="/" element={ <Navigation /> }>
+        <Route path="Home" element={<Home/>}/>
+      </Route>
     </Routes>
     
 )
