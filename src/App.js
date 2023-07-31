938c1f1e07a5277b17057afc9fcd6040a91faff2
ASM072 <ahmedsaadmalik8@gmail.com>
2023-08-01 00:19:22 +0500

    creating navigation bar, router link and fragment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 import Home from "./Components/routes/home/home.component";
+import { Fragment } from "react";
 
 const Navigation = () =>
 {
   return (
-    <div>
-      <Outlet/>
+    <Fragment>
+    <div className="navigation">
+      <Link className="logo"></Link>
     </div>
+      <Outlet />
+    </Fragment>
   )
 }
 
@@ -15,7 +19,7 @@ const App = () =>
   return (
     <Routes>
       <Route path="/" element={ <Navigation /> }>
-        <Route path="Home" element={<Home/>}/>
+        <Route index element={<Home/>}/>
       </Route>
     </Routes>
     
