a43a33cea9ba00d24d3a84a7bd7181e0eaf8d1aa
ASM072 <ahmedsaadmalik8@gmail.com>
2023-08-01 14:20:00 +0500

    fixed logo which was not working the svg image was not working

--- src/Components/routes/navigation/navigation.component.jsx
+++ src/Components/routes/navigation/navigation.component.jsx
@@ -1,6 +1,6 @@
 import { Outlet, Link } from "react-router-dom";
 import { Fragment } from "react";
-import { ReactComponent as Logo } from "../../../assets/logo.svg";
+import { ReactComponent as Logo } from "../../../assets/logo1.svg";
 import './navigation.style.scss'
 
 const Navigation = () =>
@@ -9,11 +9,14 @@ const Navigation = () =>
         <Fragment>
             <div className="navigation">
                 <Link className="logoBox" to="/">
-                    <Logo className="logo" />
+                    
+                    <Logo />
                 </Link>
+                <div className="signIN">
                 <Link className="signIn-link" to="/signIn">
                     Sign In   
                 </Link>
+                </div>
             </div>
             <Outlet />
         </Fragment>
