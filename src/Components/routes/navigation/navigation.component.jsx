e7b8b7eacc1ba86ca73a2b3184013a570aa88e04
ASM072 <ahmedsaadmalik8@gmail.com>
2023-08-01 13:55:03 +0500

    styling navigation bar

--- src/Components/routes/navigation/navigation.component.jsx
+++ src/Components/routes/navigation/navigation.component.jsx
@@ -1,8 +1,7 @@
 import { Outlet, Link } from "react-router-dom";
 import { Fragment } from "react";
-import { ReactComponent as Logo } from "./assets/logo.svg";
-z
-
+import { ReactComponent as Logo } from "../../../assets/logo.svg";
+import './navigation.style.scss'
 
 const Navigation = () =>
 {
@@ -12,8 +11,13 @@ const Navigation = () =>
                 <Link className="logoBox" to="/">
                     <Logo className="logo" />
                 </Link>
+                <Link className="signIn-link" to="/signIn">
+                    Sign In   
+                </Link>
             </div>
             <Outlet />
         </Fragment>
     )
 }
+
+export default Navigation;
