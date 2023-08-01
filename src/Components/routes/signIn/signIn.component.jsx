d6847dd557e3e958701f28026dc4602655db2852
ASM072 <ahmedsaadmalik8@gmail.com>
2023-08-01 17:29:52 +0500

    created firebase utility and used google authentication to sign in which is not currectly working i.e not updating on firebase

--- src/Components/routes/signIn/signIn.component.jsx
+++ src/Components/routes/signIn/signIn.component.jsx
@@ -1,10 +1,19 @@
+// eslint-disable-next-line
+import { signInWithGooglePopup, userDocFromAuth } from "../../../utility/firebase/firebase.utility";
+
 const SignIn = () =>
 {
+    const logGoogleUser = async () =>
+    {
+        const { user } = await signInWithGooglePopup();
+        const userDocRef = await userDocFromAuth( user );
+    };
+
     return (
         <div>
-            
+            <button onClick={ logGoogleUser }>Sign in</button>
         </div>
-    )
-}
+    );
+};
 
-export default SignIn
+export default SignIn;
