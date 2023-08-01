148050c21f4d348170a90bf7a10e8a9cda4ae023
ASM072 <ahmedsaadmalik8@gmail.com>
2023-08-01 13:54:42 +0500

    styling the navigation bar and creating a sign in page component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/routes/home/home.component";
-import { Navigation } from "react-router-dom";
+import Navigation from "./Components/routes/navigation/navigation.component";
+import SignIn from "./Components/routes/signIn/signIn.component";
 
 const App = () =>
 {
@@ -8,6 +9,7 @@ const App = () =>
     <Routes>
       <Route path="/" element={ <Navigation /> }>
         <Route index element={<Home/>}/>
+        <Route path="signIn" element={<SignIn/>}/>
       </Route>
     </Routes>
     
