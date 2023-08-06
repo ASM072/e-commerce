32208b142acd3dcc28207290a8e8f41eebb489a3
ASM072 <ahmedsaadmalik8@gmail.com>
2023-08-06 22:19:24 +0500

    creating cart item component to the item which can be added to cart, making add to cart function

--- src/Components/cart/cart.jsx
+++ src/Components/cart/cart.jsx
@@ -1,10 +1,17 @@
 import Button from '../buttons/button';
 import './cart.scss';
+import CartItem from '../cartitem/cartitem';
+
+
+
 const Cart = () =>
 {
     return (
         <div className='cartDropDownContainer'>
-            <div className='cartItems'/>
+            <div className='cartItems'>
+                {[].map(item => <CartItem cartItem={item}/>)}
+            </div>
+            <h1>cart is empty</h1>
             <Button>Go To Checkout </Button>
         </div>
     )
