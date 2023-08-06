d93af2452e170e51fcd4ef839a766932f37e86d9
ASM072 <ahmedsaadmalik8@gmail.com>
2023-08-06 22:19:33 +0500

    Cart item component

--- src/context/cartcontext.jsx
+++ src/context/cartcontext.jsx
@@ -1,13 +1,25 @@
 import { createContext, useState } from "react";
 
+
+const addCartItem = ( cartItems, productToAdd ) =>
+{
+    
+}
 export const CartContext = createContext( {
     openCart: false,
-    setOpenCart:()=>{}
+    setOpenCart: () => { }, 
+    cartItems: [],
+    addItemToCart: () => { },
 } )
 
 export const CartProvider = ( { children } ) =>
 {
     const [ openCart, setOpenCart ] = useState( false );
+    const [ cartItems, setCartItems ] = useState( [] );
+    const addItemToCart = (productToAdd) =>
+    {
+        setCartItems( addCartItem( cartItems, productToAdd ) );
+    }
     const value = { openCart, setOpenCart };
     return <CartContext.Provider value={value}>{ children }</CartContext.Provider>
     
