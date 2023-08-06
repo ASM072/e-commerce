820feb5974a67668ac6f2260b6de32994cbec22b
ASM072 <ahmedsaadmalik8@gmail.com>
2023-08-06 22:52:55 +0500

    creating items that can be added to cart

--- src/context/cartcontext.jsx
+++ src/context/cartcontext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 
 const addCartItem = ( cartItems, productToAdd ) =>
@@ -15,17 +15,26 @@ export const CartContext = createContext( {
     setOpenCart: () => { }, 
     cartItems: [],
     addItemToCart: () => { },
+    cartCount:0
 } )
 
 export const CartProvider = ( { children } ) =>
 {
     const [ openCart, setOpenCart ] = useState( false );
     const [ cartItems, setCartItems ] = useState( [] );
+    const [ cartCount, setCartCount ] = useState( 0 );
+
+    useEffect( () =>
+    {
+        const newCartCount = cartItems.reduce( ( total, cartItem ) => total + cartItem.quantity, 0 )
+        setCartCount( newCartCount );
+    }, [ cartItems ] );
+
     const addItemToCart = (productToAdd) =>
     {
         setCartItems( addCartItem( cartItems, productToAdd ) );
     }
-    const value = { openCart, setOpenCart, addItemToCart, cartItems };
+    const value = { openCart, setOpenCart, addItemToCart, cartItems, cartCount };
     return <CartContext.Provider value={value}>{ children }</CartContext.Provider>
     
 }
