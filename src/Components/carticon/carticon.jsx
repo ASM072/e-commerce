f969b9354c8a7f83ea82f8bb8093be55c5dbe34c
ASM072 <ahmedsaadmalik8@gmail.com>
2023-08-06 17:25:59 +0500

    bug fixes, added a shopping cart icon, and created a separate component for cart icon which will show the cart

---
+++ src/Components/carticon/carticon.jsx
@@ -0,0 +1,12 @@
+import { ReactComponent as ShoppingCartIcon } from "../../assets/shopping.svg";
+import './carticon.scss';
+
+const CartIcon = () =>
+{
+    return (
+        <div className="cartIconContainer">
+            <ShoppingCartIcon className="shoppingIcon"></ShoppingCartIcon>
+            <span className="itemCount"></span>
+        </div>
+    )
+}
